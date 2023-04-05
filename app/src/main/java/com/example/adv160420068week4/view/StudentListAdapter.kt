76371ea5d467762de5b73fa6c77494677c6ceae7
Stenael <stevenputranatanael@gmail.com>
2023-04-05 13:36:16 +0700

    init2

--- app/src/main/java/com/example/adv160420068week4/view/StudentListAdapter.kt
+++ app/src/main/java/com/example/adv160420068week4/view/StudentListAdapter.kt
@@ -4,11 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.example.adv160420068week4.R
 import com.example.adv160420068week4.model.Student
+import com.example.adv160420068week4.util.loadImage
 
 class StudentListAdapter(val studenList:ArrayList<Student>)
     :RecyclerView.Adapter<StudentListAdapter.StudentViewHolder>(){
@@ -35,6 +38,9 @@ class StudentListAdapter(val studenList:ArrayList<Student>)
             val action = StudentListFragmentDirections.actionStudentDetail()
             Navigation.findNavController(it).navigate(action)
         }
+        var imageView = holder.view.findViewById<ImageView>(R.id.imageView)
+        var progressBar = holder.view.findViewById<ProgressBar>(R.id.progressbar)
+        imageView.loadImage(studenList[position].photoUrl, progressBar)
     }
     fun updateStudentList(newStudentList: ArrayList<Student>) {
         studenList.clear()
